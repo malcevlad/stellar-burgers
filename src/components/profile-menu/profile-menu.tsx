a0f047a6e6e8d737fbfde2328be6b1b70edfdc04
Vladislav Maltsev <malcelvad777@yandex.ru>
2024-09-30 18:34:31 +0300

    11-th project first review

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,11 +1,18 @@
 import { FC } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { ProfileMenuUI } from '@ui';
+import { useDispatch } from '../../services/store';
+import { logoutUser } from '../../services/slices/userSlice';
 
 export const ProfileMenu: FC = () => {
   const { pathname } = useLocation();
 
-  const handleLogout = () => {};
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    <Navigate replace to='/login' />;
+  };
 
   return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
 };
--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,18 +1,27 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import {
+  errorTextSelector,
+  registerUser
+} from '../../services/slices/userSlice';
+import { useDispatch, useSelector } from '../../services/store';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const dispatch = useDispatch();
+  const errorText = useSelector(errorTextSelector);
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(registerUser({ email, name: userName, password }));
   };
 
   return (
     <RegisterUI
-      errorText=''
+      errorText={errorText}
       email={email}
       userName={userName}
       password={password}
